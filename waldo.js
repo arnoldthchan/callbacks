6c6c86a6b5caa1ab8e2a40c40a94cd930f3d6b58
arnoldthchan <arnoldthchan@gmail.com>
2017-06-29 13:01:51 +0000

    refactor to use forEach instead of for loop

--- waldo.js
+++ waldo.js
@@ -1,10 +1,15 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
-    }
-  }
+  // for (var i = 0; i < arr.length; i++) {
+  //   if (arr[i] === "Waldo") {
+  //     found(i);   // execute callback
+  //   }
+  // }
+  arr.forEach(function(i){
+      if (i === "Waldo") {
+        found(arr.indexOf(i));   // execute callback
+      }
+  })
 }
 
 function actionWhenFound(indexNum) {
@@ -12,3 +17,4 @@ function actionWhenFound(indexNum) {
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
