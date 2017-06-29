9b348c9384869dac0dc4248afda6476a7ca69edd
arnoldthchan <arnoldthchan@gmail.com>
2017-06-29 16:02:04 +0000

    declared all variables

--- pythag.js
+++ pythag.js
@@ -1,3 +1,5 @@
+// 'use strict';
+
 var input = [
   { x: 3, y: 4 },
   { x: 12, y: 5 },
@@ -5,11 +7,10 @@ var input = [
 ];
 
 var result = input.map(function(num){
-  srqRoot = 0;
   // console.log('numbers:', num.x, num.y);
-  total = (Math.pow(num.x, 2) + Math.pow(num.y, 2));
+  var total = (Math.pow(num.x, 2) + Math.pow(num.y, 2));
   // console.log('total:', total);
-  srqRoot = Math.sqrt(total);
+  var srqRoot = Math.sqrt(total);
   return (srqRoot);
 });
 
